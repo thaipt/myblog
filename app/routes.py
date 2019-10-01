0aeb0b22051f0beae9e55e7a5bb4e2faba22c0eb
Thai Pham <thaipt@gmail.com>
2019-10-01 16:29:23 -0600

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Thai'}
+    posts = [
+        {
+            'author': {'username': 'Nguyen'},
+            'body': 'Flask de hoc qua phai khong?'
+        },
+        {
+            'author': {'username': 'Long'},
+            'body': 'Lap trinh Web that thu vi!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
