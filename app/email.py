c87e94f7ddbd2ebddbf003009cf20f25df420bb5
Thai Pham <thaipt@gmail.com>
2019-10-18 15:48:20 -0600

    Chapter 13: I18n and L10n

--- app/email.py
+++ app/email.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask_mail import Message
 from app import mail, app
 from threading import Thread
+from flask_babel import _
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -15,7 +16,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Microblog] Reset Your Password',
+    send_email(_('[Myblog] Reset Your Password'),
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'vi']
