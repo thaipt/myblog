f3cc160512cd69bbe32ce4f566997edb10341b4b
Thai Pham <thaipt@gmail.com>
2020-01-03 16:17:55 -0700

    Chapter 21: User Notification and Private Message

--- myblog.py
+++ myblog.py
@@ -1,9 +1,10 @@
 from app import create_app, db, cli
-from app.models import User, Post
+from app.models import User, Post, Message, Notification
 
 app = create_app()
 cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+            'Notification': Notification}
